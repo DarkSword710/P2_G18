3ef11bb4bfa1dcd8294d0aa273361511238703e8
adriaortizmartinez <adriaortizmartinez@S3-03>
2017-04-18 11:36:24 +0200

    vectores

---
+++ Alchemy/main.cc
@@ -0,0 +1,8 @@
+#include <fstream>
+#include <unordered_map>
+#include <vector>
+
+
+std::unordered_map<std::string, std::string>;
+std::vector<std::string>basics({ "Air", "Earth", "Fire", "Water" });
+std::vector<std::string>inventory(basics);
